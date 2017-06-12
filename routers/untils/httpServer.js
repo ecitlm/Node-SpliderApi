e837af3004ad6ba6beaee406cb9a3289f25c5d60
ecitlm <ecitlm@163.com>
2017-06-12 12:48:46 +0800

    update http request

--- routers/untils/httpServer.js
+++ routers/untils/httpServer.js
@@ -2,52 +2,46 @@
  * @Author: ecitlm 
  * @Date: 2017-06-07 16:22:29 
  * @Last Modified by: ecitlm
- * @Last Modified time: 2017-06-11 23:24:49
+ * @Last Modified time: 2017-06-12 12:48:14
  */
 const express = require('express')
 var http = require('http')
 const app = express()
 const querystring = require("querystring");
+const request = require("request");
+
 
 /**
- * 
- * @param {string} host 域名
- * @param {string} url  路径 
- * @param {string} method 请求方式
- * @param {object} data  请求参数
- * @param {string} bool true :https ,false:http 判断是http 还是https请求
+ * http get网络请求封装
+ * @param {any} 域名 
+ * @param {any} 参数 
+ * @param {any} 接口路径 
+ * @param {bool} true false 是否为https
  * @returns 
  */
+function httpGet(host, data, path, status) {
 
-function httpServer(host, path, method, data, status) {
-    console.log('-------------------------------------');
-    console.log(data);
-     if (method == 'GET') {
-           var path=path+"&"+ querystring.stringify(data);
-        }
-        console.log(path)
+    //var path=path+"&"+querystring.stringify(data);
+    console.log("===================HttpGet=====================");
     var options = {
-        hostname: host,
+        host: host,
         port: 8097,
-        path: path,
-        method: method,
+        path: path + querystring.stringify(data),
+        method: 'GET',
         headers: {
-            "Content-Type":"application/json",
+            'Content-Type': 'application/json',
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36"
-        }//伪造请求头
+        }
     };
+    //判断是否为https请求
     if (status) {
-        http = require('https')
+        http = require('https');
         options.port = 443
     }
-   if (method == 'POST') {
-          options.body=data;
-            console.log(options)
-        }
-      
+
     return new Promise(function (resolve, reject) {
         let body = "";
-        var httpRequest = http.request(options, function (response) {
+        var get_req = http.request(options, function (response) {
             response.on("data", function (chunk) {
                 body += chunk;
             })
@@ -59,22 +53,66 @@ function httpServer(host, path, method, data, status) {
             response.on('error', err => {
                 reject(err)
             })
+
         })
 
-        if (method == 'POST') {
-           // httpRequest.write(data)
-           //console.log(require('querystring').stringify(data))
-           //httpRequest.write(require('querystring').stringify(data))
-        }
-          
-        
-        httpRequest.end();
-     
-    })
+        get_req.end();
+
+    });
 }
- 
 
 
+/**
+ * http POST 请求
+ * @param {string} 域名 
+ * @param {any} 参数 
+ * @param {any} 接口路径 
+ * @param {bool} true false 是否为https
+ * @returns 
+ */
+function httpPost(host, data, path, status) {
+    var data = querystring.stringify(data);
+    console.log("---------httpPost---------------")
+    console.log(data)
+    var options = {
+        host: '192.168.1.2',
+        port: '8097',
+        path: path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
+            'Content-Length': Buffer.byteLength(data)
+        }
+    }
+    //判断是否为https请求
+    if (status) {
+        http = require('https');
+        options.port = 443
+    }
+    return new Promise(function (resolve, reject) {
+        let body = "";
+        var post_req = http.request(options, function (response) {
+            response.on("data", function (chunk) {
+                body += chunk;
+            })
+
+            response.on('end', () => {
+                resolve(body)
+            })
+
+            response.on('error', err => {
+                reject(err)
+            })
+
+        })
+
+        post_req.write(data);
+        post_req.end();
+    })
+
+}
 module.exports = {
-    httpServer
+    httpGet,
+    httpPost
 }
--- routers/untils/post_test.js
+++ routers/untils/post_test.js
@@ -4,14 +4,13 @@ const Server = require('./httpServer.js')
 const querystring = require("querystring");
 
 //http://192.168.1.2:8097/Public/?service=Index.index
+
 app.get('/:page', function (req, res) {
     var host = "192.168.1.2";
-    console.log(req.params)
-    var path="/Public/?service=Index.query";
-    var method = "POST"; //POST GET
-    var data =req.params// querystring.stringify(req.params)
+    var path="/Public/?service=Index.query&";
+    var data =req.params
     //false:http请求  true:https请求
-    Server.httpServer(host, path, method, data,false).then(function (body) {
+    Server.httpGet(host,data,path,false).then(function (body) {
         res.send(JSON.parse(body));
 
     }).catch(function (err) {
