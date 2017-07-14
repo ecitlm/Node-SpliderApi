290571ca38822eaa33332a4acff23a09ac250623
ecitlm <ecitlm@163.com>
2017-07-14 14:41:06 +0800

    update routes fileName

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: ecitlm 
  * @Date: 2017-06-19 14:50:54 
  * @Last Modified by: ecitlm
- * @Last Modified time: 2017-07-14 11:42:29
+ * @Last Modified time: 2017-07-14 14:39:41
  */
 
 const express = require('express');
@@ -22,20 +22,20 @@ app.all('*', function(req, res, next) {
 
 /**路由列表**/
 const index = require('./index');
-const classify = require('./routers/picture/classify');
-const classify_tags_list = require('./routers/picture/classify_tags_list');
-const list = require('./routers/picture/list');
-const img_view = require('./routers/picture/img_view');
-const daily_list = require('./routers/web_daily/daily_list');
-const recommend_list = require('./routers/web_daily/recommend');
-const one_day_list = require('./routers/web_daily/one_day_list');
-const web_frame = require('./routers/web_frame/hot_frame');
-const zhihu_news = require('./routers/zhihu_daily/zhihu_news');
-const zhihu_news_detail = require('./routers/zhihu_daily/news_detail');
+const classify = require('./routes/picture/classify');
+const classify_tags_list = require('./routes/picture/classify_tags_list');
+const list = require('./routes/picture/list');
+const img_view = require('./routes/picture/img_view');
+const daily_list = require('./routes/web_daily/daily_list');
+const recommend_list = require('./routes/web_daily/recommend');
+const one_day_list = require('./routes/web_daily/one_day_list');
+const web_frame = require('./routes/web_frame/hot_frame');
+const zhihu_news = require('./routes/zhihu_daily/zhihu_news');
+const zhihu_news_detail = require('./routes/zhihu_daily/news_detail');
 
 app.use('/', index);
 //图片列表
-app.use('/huaban', require('./routers/picture/huaban'));
+app.use('/huaban', require('./routes/picture/huaban'));
 app.use('/classify', classify);
 app.use('/classify_tags_list', classify_tags_list);
 app.use('/list', list);
@@ -53,22 +53,22 @@ app.use('/zhihu_news', zhihu_news);
 app.use('/zhihu_news_detail', zhihu_news_detail);
 
 //新闻头条
-app.use('/news_list', require('./routers/news_toutiao/news_list'));
-app.use('/news_detail', require('./routers/news_toutiao/news_detail'));
-app.use('/video_list', require('./routers/news_toutiao/video_list'));
+app.use('/news_list', require('./routes/news_toutiao/news_list'));
+app.use('/news_detail', require('./routes/news_toutiao/news_detail'));
+app.use('/video_list', require('./routes/news_toutiao/video_list'));
 
 //music router
-app.use('/music_list', require('./routers/music/music_list'));
-app.use('/new_songs', require('./routers/music/new_songs'));
-app.use('/rank_list', require('./routers/music/rank_list'));
-app.use('/rank_info_list', require('./routers/music/rank_info_list'));
+app.use('/music_list', require('./routes/music/music_list'));
+app.use('/new_songs', require('./routes/music/new_songs'));
+app.use('/rank_list', require('./routes/music/rank_list'));
+app.use('/rank_info_list', require('./routes/music/rank_info_list'));
 
 
 //joke
-app.use('/joke_pic', require('./routers/joke/joke_pic'));
-app.use('/joke', require('./routers/joke/joke'));
+app.use('/joke_pic', require('./routes/joke/joke_pic'));
+app.use('/joke', require('./routes/joke/joke'));
 
-app.use('/post_test', require('./routers/untils/post_test'));
+app.use('/post_test', require('./routes/untils/post_test'));
 app.use(router);
 app.listen(3000);
 console.log(3000);
