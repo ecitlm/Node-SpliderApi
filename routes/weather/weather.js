6d949445a9edc4c1009a0be0dfce071895589ab6
ecitlm <ecitlm@163.com>
2017-07-20 17:13:46 +0800

    update

--- routes/weather/weather.js
+++ routes/weather/weather.js
@@ -2,7 +2,7 @@
  * @Author: ecitlm 
  * @Date: 2017-07-20 10:06:22 
  * @Last Modified by: ecitlm
- * @Last Modified time: 2017-07-20 10:33:40
+ * @Last Modified time: 2017-07-20 17:11:33
  */
 //天气预报
 
