e64962feaf7b78050947f9eeb1a986445b80bf14
ecitlm <ecitlm@163.com>
2017-12-05 20:53:11 +0800

    add init

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var ejs = require('ejs');
 
 
-app.engine('html',ejs.__express);
-app.set('view engine','html');
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 app.set('views', './views'); //设置模板路径
 //app.set('view engine', '.html');
 
@@ -12,7 +12,7 @@ app.set('views', './views'); //设置模板路径
 //app.set('views', __dirname + 'views');
 
 app.get('/', function(req, res) {
-   // res.header("Content-Type:text/html; charset=utf-8");
+    // res.header("Content-Type:text/html; charset=utf-8");
     res.render('index', {
         title: '欢迎进入IT开发者--Nodejs云服务平台'
     });
