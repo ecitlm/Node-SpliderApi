23020271a5d2d221fd2d2c9d01810b753e6592f2
ecitlm <ecitlm@163.com>
2022-05-24 12:11:45 +0800

    🐞 fix(swaggerUI):fix swaggerUI api url

--- src/controller/tang300.js
+++ src/controller/tang300.js
@@ -2,13 +2,13 @@ const query = require('@/src/models/shici/tang300');
 const app = require('express')();
 
 /**
- * GET /api/ts300
+ * GET /api/tang300
  * @summary 唐诗300首
  * @description 唐诗300首
  * @param {string}  contents.query.required  - 关键字
  * @param {number}  page.query.required  - 页码
  */
-app.get('/', function (req, res) {
-  query(req, res);
+app.get('/', async function (req, res) {
+  await query(req, res);
 });
 module.exports = app;
